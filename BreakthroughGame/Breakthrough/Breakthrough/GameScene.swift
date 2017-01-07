dd44a59f59d356451d8ca8aac4db706af00a460e
Ryan Lee <rlee33@gmail.com>
2017-01-07 17:49:01 -0600

    added touch functionality

--- BreakthroughGame/Breakthrough/Breakthrough/GameScene.swift
+++ BreakthroughGame/Breakthrough/Breakthrough/GameScene.swift
@@ -11,79 +11,83 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball = SKSpriteNode()
+    var leftBumper = SKSpriteNode()
+    var rightBumper = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        leftBumper = self.childNode(withName: "leftFlipper") as! SKSpriteNode
+        rightBumper = self.childNode(withName: "rightFlipper") as! SKSpriteNode
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            
+            if touchLocation.x < 0 {
+                //flip up left bumper
+                print("touched left")
+                
+//                let flip = [SKAction.run({() -> Void in
+//                }), SKAction.rotate(toAngle: -45 * .pi / 180, duration: 0.1), SKAction.run({() -> Void in
+//                })]
+//                
+//                self.leftBumper.run(SKAction.sequence(flip))
+                
+                self.leftBumper.run(SKAction.rotate(toAngle: -45, duration: 0.8))
+                
+            } else if touchLocation.x > 0 {
+                //flip up right bumper
+                print("touched right")
+                self.rightBumper.run(SKAction.rotate(toAngle: -45, duration: 0.8))
+            }
+            
+            
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for touch in touches {
+            if touch.location(in: self).x < 0 {
+                self.leftBumper.run(SKAction.rotate(toAngle: 0, duration: 0.5))
+            } else if touch.location(in: self).x > 0 {
+                self.rightBumper.run(SKAction.rotate(toAngle: 0, duration: 0.5))
+            }
+            
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
 }
+
+
+//// Touch on Flippers logic
+//if touchLocation.x > 188 && touchLocation.y < 500 {
+//    //        NSLog(@"Right Flipper Tapped");
+//    var sequence = [SKAction.runBlock({() -> Void in
+//        self.rightFlipperActive = true
+//    }), SKAction.rotate(toAngle: -45 * .pi / 180, duration: 0.1), SKAction.runBlock({() -> Void in
+//        self.rightFlipperActive = false
+//    })]
+//    self.rightFlipper.runAction(SKAction.sequence(sequence))
+//}
+//if touchLocation.x < 188 && touchLocation.y < 500 {
+//    //        NSLog(@"Left Flipper Tapped");
+//    var sequence = [SKAction.runBlock({() -> Void in
+//        self.leftFlipperActive = true
+//    }), SKAction.rotate(toAngle: +45 * .pi / 180, duration: 0.1), SKAction.runBlock({() -> Void in
+//        self.leftFlipperActive = false
+//    })]
+//    self.leftFlipper.runAction(SKAction.sequence(sequence))
+//}
