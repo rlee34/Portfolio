2f9561bdc2590f78427e8cc7b5589fa3cf84cc50
Ryan Lee <rlee33@gmail.com>
2017-04-15 17:49:03 -0500

    Twittler init

---
+++ Twittler/Twittler/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Twittler
+//
+//  Created by Ryan Lee on 4/15/17.
+//  Copyright © 2017 Ryan Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
