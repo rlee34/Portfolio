7695d9cd3c10d017d75898492c4aa65635673601
Ryan Lee <rlee33@gmail.com>
2017-04-16 22:27:17 -0500

    add arrays of words to be used for random tweet generation

--- Twittler/Twittler/DataGenerator.swift
+++ Twittler/Twittler/DataGenerator.swift
@@ -22,4 +22,12 @@ struct User {
     var tweets: [String]
 }
 
+//random tweet generator
+
+var opening = ["just", "", "", "", "", "ask me how I", "completely", "nearly", "productively", "efficiently", "last night I", "the president", "that wizard", "a ninja"]
+var verbs = ["drank", "drunk", "deployed", "got", "developed", "built", "invented", "experienced", "fought off", "hardened", "enjoyed", "developed", "consumed", "debunked", "drugged", "doped", "made", "wrote", "saw"]
+var objects = ["my", "your", "the", "a", "my", "an entire", "this", "that", "the", "the big", "a new form of"]
+var nouns = ["cat", "koolaid", "system", "city", "worm", "cloud", "potato", "money", "way of life", "belief system", "security system", "bad decision", "future", "life", "pony", "mind"]
+var tags = ["#techlife", "#burningman", "#sf", "but only I know how", "for real", "#sxsw", "ballin'", "#omg", "#yolo", "#magic", "", "", "", ""]
+
 
