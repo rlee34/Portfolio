0897aaaa6f30f89672a4dd265d219f16b99a7c32
Ryan Lee <rlee33@gmail.com>
2017-05-03 21:53:14 -0500

    WeatherDen init

---
+++ WeatherDen/WeatherDen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherDen
+//
+//  Created by Ryan Lee on 5/3/17.
+//  Copyright © 2017 Ryan Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
